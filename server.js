59114c4124abebde2db2557c12c79b24b9e26710
jasimSalman <jassimmo7amed+1@gmail.com>
2024-07-04 13:26:59 +0300

    initial controllers & routers

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ const PORT = process.env.PORT || 3001
 
 require('./config/database')
 
+const categoryRouter = require('./routes/category')
+
 const app = express()
 
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use('/category', categoryRouter)
 
 app.listen(PORT, () => {
   console.log(`Running Express server on Port ${PORT} . . .`)
