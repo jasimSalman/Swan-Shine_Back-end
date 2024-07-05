b429316a77eb233b209303b82cf8d64d2119aed5
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-07-05 12:06:46 +0300

    updated the controller to receive the json data

--- controllers/category.js
+++ controllers/category.js
@@ -2,7 +2,13 @@ const axios = require('axios')
 const Category = require('../models/Category')
 
 const index = async (req, res) => {
-  const categories = await Category.find({})
+  try {
+    const categories = await Category.find({})
+    res.json(categories) // Send categories as JSON response
+  } catch (err) {
+    console.error('Error fetching categories', err)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 }
 
 module.exports = {
