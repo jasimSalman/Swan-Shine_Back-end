ccc4a8298a3922a6d587f17784dac1d0d40bb25a
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-07-06 23:58:40 +0300

    done with CRD functions for wishlist

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const Item = require('../models/Item')
 
+//This function allows the user to add an item to his wishlist
 const addToWishlist = async (req, res) => {
   const userId = req.params.userId
   const itemId = req.params.itemId
@@ -28,8 +29,47 @@ const addToWishlist = async (req, res) => {
     console.error(err)
     res.status(500).send({ message: 'Internal server error' })
   }
+} // http://localhost:3001/wishlist/{userId}/{itemId}
+
+//This function shows the User's wishlist
+const showWishlist = async (req, res) => {
+  const userId = req.params.userId
+  try {
+    const user = await User.findById(userId).populate('fav_list')
+
+    if (!user) {
+      return res.status(404).send({ message: 'User not found !' })
+    }
+    res.status(200).send(user.fav_list)
+  } catch (err) {
+    res.status(500).send({ message: 'Internal Server error' })
+  }
+} // http://localhost:3001/wishlist/:userId
+
+//This function will remove an item from the user's wishlist
+const removeFromWishlist = async (req, res) => {
+  const userId = req.params.userId
+  const itemId = req.params.itemId
+
+  // console.log(`Removing item ${itemId} from user ${userId}'s wishlist`)
+  try {
+    const user = await User.findById(userId)
+
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' })
+    }
+    user.fav_list.pull(itemId)
+    await user.save()
+
+    res.status(200).send(user)
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({ message: 'Internal server error' })
+  }
 } // http://localhost:3001/wishlist/:userId/:itemId
 
 module.exports = {
-  addToWishlist
+  addToWishlist,
+  showWishlist,
+  removeFromWishlist
 }
