28b10bf79690d52f9a434684254f204889423be9
jasimSalman <jassimmo7amed+1@gmail.com>
2024-07-06 21:11:33 +0300

    create delete item from cart function

--- controllers/cart.js
+++ controllers/cart.js
@@ -54,6 +54,40 @@ const addToCart = async (req, res) => {
   }
 } //http://localhost:3001/cart/:userId
 
+//This function will delete a specific item from the currect cart.
+const deleteFromCart = async (req, res) => {
+  const userId = req.params.userId
+  const itemId = req.params.itemId
+
+  try {
+    const user = await User.findById(userId)
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' })
+    }
+
+    let cart = await Cart.findOne({ user: userId, checked_out: false })
+    if (!cart) {
+      return res.status(404).json({ error: 'No current cart found' })
+    }
+
+    const itemIndex = cart.items.findIndex(
+      (item) => item.item.toString() === itemId
+    )
+    if (itemIndex === -1) {
+      return res.status(404).json({ error: 'Item not found in the cart' })
+    }
+
+    cart.items.splice(itemIndex, 1)
+    await cart.save()
+
+    res.json(cart)
+  } catch (err) {
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+} //http://localhost:3001/cart/:userId/:itemId
+
 module.exports = {
-  addToCart
+  addToCart,
+  deleteFromCart,
+  index
 }
--- routes/cart.js
+++ routes/cart.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const cartCtrl = require('../controllers/cart')
 
+router.get('/:userId', cartCtrl.index)
+
 router.post('/:userId', cartCtrl.addToCart)
+
+router.delete('/:userId/:itemId', cartCtrl.deleteFromCart)
 
 module.exports = router
