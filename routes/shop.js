0e14d103db1c299e495519781b991e51c784d21b
jasimSalman <jassimmo7amed+1@gmail.com>
2024-07-07 12:12:10 +0300

    add the JWT auth to the routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const shopCtrl = require('../controllers/shop')
+const middleware = require('../middleware')
 
-router.get('/', shopCtrl.index)
-router.get('/:shopId/items', shopCtrl.getItemsByShop)
+router.get('/', middleware.stripToken, middleware.verifyToken, shopCtrl.index)
+router.get(
+  '/:shopId/items',
+  middleware.stripToken,
+  middleware.verifyToken,
+  shopCtrl.getItemsByShop
+)
 
 module.exports = router
--- routes/wishlist.js
+++ routes/wishlist.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const wishlistController = require('../controllers/wishlist')
+const middleware = require('../middleware')
 
-router.post('/:userId/:itemId', wishlistController.addToWishlist)
-router.get('/:userId', wishlistController.showWishlist)
-router.delete('/:userId/:itemId', wishlistController.removeFromWishlist)
+router.post(
+  '/:userId/:itemId',
+  middleware.stripToken,
+  middleware.verifyToken,
+  wishlistController.addToWishlist
+)
+router.get(
+  '/:userId',
+  middleware.stripToken,
+  middleware.verifyToken,
+  wishlistController.showWishlist
+)
+router.delete(
+  '/:userId/:itemId',
+  middleware.stripToken,
+  middleware.verifyToken,
+  wishlistController.removeFromWishlist
+)
 
 module.exports = router
